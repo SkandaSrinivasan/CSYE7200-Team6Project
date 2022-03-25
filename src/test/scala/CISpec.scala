9c6fefdd1ee998fa72982498676641fd896e2a4a
Skanda Srinivasan <srinivasan.sk@northeastern.edu>
2022-03-25 16:53:11 -0400

    fixed spec

--- src/test/scala/CISpec.scala
+++ src/test/scala/CISpec.scala
@@ -3,8 +3,8 @@ import org.scalatest.matchers.must.Matchers
 
 class CISpec extends AnyFlatSpec with Matchers{
 
-  behavior of "Project"
-  it should "Driver.testing should return hello" in {
+  behavior of "Driver.testing"
+  it should "should return hello" in {
     Driver.testing mustEqual "Looks good";
   }
 }

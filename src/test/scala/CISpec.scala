5c7fe7a93c472893fc2e6c2a2a3457a39adc335a
Skanda Srinivasan <srinivasan.sk@northeastern.edu>
2022-03-27 11:57:54 -0400

    added test to check spark session creation

--- src/test/scala/CISpec.scala
+++ src/test/scala/CISpec.scala
@@ -1,10 +1,27 @@
+import org.apache.spark.sql.SparkSession
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.must.Matchers
+import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
+
+import scala.util.{Success, Try}
 
 class CISpec extends AnyFlatSpec with Matchers{
+  val spark: SparkSession = SparkSession
+    .builder()
+    .appName("Spark Assignment6")
+    .master("local[*]")
+    .getOrCreate();
 
   behavior of "Driver.testing"
   it should "should return hello" in {
     Driver.testing mustEqual "Looks good";
   }
+
+  it should "show success when creating spark session" in {
+
+    Try(spark) should matchPattern {
+      case Success(s:SparkSession) =>
+    }
+  }
+
 }

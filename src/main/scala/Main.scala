6abf9734d0431ff3036b581ee4833d5b39bed11f
Skanda Srinivasan <srinivasan.sk@northeastern.edu>
2022-04-04 22:13:34 -0400

    Converted DF to Dataset so now its all strongly typed :D

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,10 @@
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+import org.apache.spark.sql.functions.{col, length}
 
-class Main {
-
-}
+class Main {}
 
 object Main extends App {
+
   val spark = SparkSession
     .builder()
     .appName("Spark Assignment6")
@@ -13,9 +13,23 @@ object Main extends App {
 
   spark.sparkContext.setLogLevel("ERROR")
 
-    val df = spark.read
-      .option("header", "true")
-      .csv("src/main/resources/vaccination_all_tweets.csv");
+  import spark.implicits._;
 
-    println(df.count())
+  val df: DataFrame = spark.read
+    .option("header", "true")
+    .csv("src/main/resources/vaccination_all_tweets.csv");
+
+  val cleaned_df: DataFrame =
+    df.dropDuplicates(Seq("text"))
+      .na
+      .drop(Seq("text"))
+      .filter(
+        length($"text") > 4
+      ) //Tweets that exceed atleast 3 characters seems reasonable
+
+  //Lets convert our Dataframe to a Dataset now
+  val tweets: Dataset[Tweet] = df.select("text", "user_location").as[Tweet]
+  tweets
+    .filter(tweet => tweet.text != null && tweet.user_location != null)
+    .show()
 }
